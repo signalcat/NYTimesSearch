a94ad8c68a79e0939d23c5ec08e278b15f1b8ab4
He Zhang <hezhang757@gmail.com>
2017-09-18 16:35:54 -0700

    Added AyncHttpClient and got JSON object back by sending request.

--- app/src/main/java/com/codepath/nytimessearch/SearchActivity.java
+++ app/src/main/java/com/codepath/nytimessearch/SearchActivity.java
@@ -5,11 +5,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+//    GridView gvResults;
+//    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +35,9 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        etQuery = (EditText) findViewById(R.id.etQuery);
+//        gvResults = (GridView) findViewById(R.id.gvResults);
+//        btnSearch = (Button) findViewById(R.id.btnSearch);
     }
 
     @Override
@@ -40,5 +60,24 @@ public class SearchActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+//        Toast.makeText(this, "Searching for" + query, Toast.LENGTH_LONG).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+        RequestParams params = new RequestParams();
+        params.put("api-key","b006b8bc57b343a490fa8ecaf1e04c97");
+        params.put("page", 0);
+        params.put("q", query);
+
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+            }
+        });
+
     }
 }
